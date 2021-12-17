651eeceb7504822848340d2bc8e0d02ecd90c039
Sonakshi <sonakshi2500@gmail.com>
2021-12-17 19:52:00 +0530

    Avatar system Added

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['DEBUG'] = True
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-from models import user
+from models import user, avatars
 
 # two decorators, same function
 @app.route('/')
@@ -22,20 +22,27 @@ def index():
 
 @app.route('/register')
 def register():
-    return render_template("register.html")
+    avatar=avatars.query.all()
+    return render_template("register.html",avatars=avatar)
 
 @app.route('/attendees')
 def attendees():
     users=user.query.all()
+    avatar=avatars.query.all()
     usr_list = []
     for u in users:
+        for av in avatar:
+            if av.id==u.avatar_id:
+                lin=av.link
+                break
         usr = {
             'id': u.id,
             'name': u.name,
             'age': u.age,
             'education': u.education,
-            'state': u.state
-        }
+            'state': u.state,
+            'avatar': lin
+            }
         usr_list.append(usr)
     return render_template("attendees.html",users=usr_list)
 
@@ -48,13 +55,14 @@ def addUser():
         age = request.form.get('age')
         education = request.form.get('education')
         state = request.form.get('state')
+        avatar = request.form.get('avatar')
 
         if name and email and phone and age and education and state:
 
             if user.query.filter_by(email=email).first():
                 return("Email ID already Exists")
             else:
-                db.session.add(user(name=name, email=email, phone=phone, age=age, education=education, state=state))
+                db.session.add(user(name=name, email=email, phone=phone, age=age, education=education, state=state, avatar_id=avatar))
                 db.session.commit()
                 return("Thank you for registering")
         else:
--- models.py
+++ models.py
@@ -9,11 +9,22 @@ class user(db.Model):
     age=db.Column(db.Integer)
     education=db.Column(db.String)
     state=db.Column(db.String)
+    
+    avatar_id = db.Column(db.Integer, db.ForeignKey('avatars.id'))
 
-    def __init__(self, name, email, phone, age, education, state):
+    def __init__(self, name, email, phone, age, education, state, avatar_id):
         self.name = name
         self.email = email
         self.phone = phone
         self.age = age
         self.education = education
         self.state = state
+        self.avatar_id = avatar_id
+
+class avatars(db.Model):
+    __tablename__="avatars"
+    id=db.Column(db.Integer, primary_key=True)
+    link=db.Column(db.String)
+
+    def __init__(self, link):
+        self.link = link
